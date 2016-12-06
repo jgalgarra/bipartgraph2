79ab58235a0b58b093c480f0ca8a9627ec8b8a3d
jmgarciasanti <jmgarciasanti@gmail.com>
2016-12-06 13:03:30 +0100

    Creacion de BAT para la instalacion de dependencias

---
+++ install_dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+set R=C:\Program Files\R\R-3.2.5
+set RSCRIPT=%R%\bin\Rscript.exe
+set R_LIBS=%USERPROFILE%\Documents\R\win-library
+cd "%~dp0"
+Echo Current directory: "%CD%"
+mkdir "%R_LIBS%"
+"%RSCRIPT%" -e "install.packages(c('ggplot2', 'gridExtra', 'vegan', 'sna', 'fields'), repos = 'http://cran.r-project.org')"
+"%RSCRIPT%" -e "install.packages('devtools', repos = 'http://cran.r-project.org')"
+"%RSCRIPT%" -e "library('devtools');install_github('jgalgarra/kcorebip')"
+"%RSCRIPT%" -e "install.packages(c('shiny', 'shinythemes', 'shinyjs'), repos = 'http://cran.r-project.org')"
+pause

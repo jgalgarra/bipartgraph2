c337497315ce43b158cb664cce3b936ed8f79732
Javier Garcia Algarra <jgalgarra@coit.es>
2017-05-11 16:42:39 +0200

    cran default repo set

--- install_bipartgraph.R
+++ install_bipartgraph.R
@@ -1,9 +1,10 @@
+rdef <- "http://cran.us.r-project.org"
 list.of.packages <- c('ggplot2', 'scales',
                       'stringr', 'grid','igraph', 'bipartite',
                       'gridExtra', 'vegan', 'sna', 'fields',
                       'shiny', 'shinythemes', 'shinyjs')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) install.packages(new.packages, repos = rdef)
 library("devtools")
 list.of.packages <- c('kcorebip')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]

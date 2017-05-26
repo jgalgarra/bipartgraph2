585d81fcb3d8fe51bc73db91586795f8b9227a3d
dummy <dummy@solydxk>
2017-05-26 20:05:49 +0200

    install_bipartgraph no longer removes kcorebip

--- install_bipartgraph.R
+++ install_bipartgraph.R
@@ -11,8 +11,8 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages, repos = rdef)
 
 # Check if kcorebip is installed and remove up to download the latest release
-plic <- data.frame(installed.packages())
-if (sum(grepl("kcorebip",plic$Package)))
-  remove.packages("kcorebip")
+#plic <- data.frame(installed.packages())
+#if (sum(grepl("kcorebip",plic$Package)))
+#  remove.packages("kcorebip")
 library("devtools")
 install_github('jgalgarra/kcorebip')

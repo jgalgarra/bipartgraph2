351f90f822f5795b40ba9c512951d3df6cf9ca76
Javier Garcia Algarra <jgalgarra@coit.es>
2017-05-28 15:59:13 +0200

    Add install_createenv for non root users

--- install_bipartgraph.R
+++ install_bipartgraph.R
@@ -1,18 +1,13 @@
 # Installation script for BipartGraph.
-
-# Create user library if it does not exist.
+print(Sys.getenv("R_LIBS_USER"))
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
-rdef <- "http://cran.us.r-project.org"
+print(.libPaths())
+
 list.of.packages <- c('ggplot2', 'scales', 'colourpicker',
                       'grid','igraph', 'bipartite',
                       'gridExtra', 'vegan', 'sna', 'fields','DT',
                       'shiny', 'shinythemes', 'shinyjs','devtools')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos = rdef)
-
-# Check if kcorebip is installed and remove up to download the latest release
-#plic <- data.frame(installed.packages())
-#if (sum(grepl("kcorebip",plic$Package)))
-#  remove.packages("kcorebip")
+if(length(new.packages)) install.packages(new.packages,repos="http://cran.us.r-project.org")
 library("devtools")
 install_github('jgalgarra/kcorebip',force=TRUE)
